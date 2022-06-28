92340acc581d56eb56eb58a0e351798829d33b1f
IdoCohen138 <ido3@post.bgu.ac.il>
2022-06-28 21:59:37 +0300

    aboutFix

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -6,22 +6,37 @@
    <div id="about_page" class="aboutClass" >
 			<div class="aboutClass-inner">
 				<center>
-				<h1 class="headline">About Us Game</h1>
+				<h1 class="headline">About Us Website</h1>
 				</center>
 				<div class="aboutSec">
-					<h2 style="font-style:italic;">Pacman game</h2>
+	
 					<p id="about_para">
-						The user can play and change the settings of number monsters, ball color, time and more as you wish.<br>
-						We encountered difficulties such as html's design issues and some functionality of the game - it took us quite some time, but The assignment 1 of building a simple site helped us to meet the goals we set for ourselves.
-						<br> used jQUERY to set functionalities such as identify details of new users to the game, set attributes dynamically to html elements, check values of elements and more...
-						in addition, We used the regex expression to make sure the fields at the registration page were entered correctly.<br>
+						On this website you can search for new or old recipes,<br>
+						you can register on the site and enjoy a culinary experience via the Internet. <br>
+						plus you can create your own new recipes, and add them to your favorites.<br><br>
+		
+						Hope You Enjoy!
+						<br>
 					</p>
+
+					<div style="display: inline-block;">
+						<div style="display: inline;">
+							<p style="color:crimson;">Link To Our First Game</p>
+						</div>
+						<div class="linkes" style="display: inline;">
+
+							<a href="https://web-development-environments-2022.github.io/assignment2-315450569_316586908/">
+								<img class="pacManIc" src="../images/pacMan-removebg-preview.png" >
+							</a>
+						</div>
+					</div>
+
 				</div>
 				<h1 id="contact" class="headline" style="text-align:center">Contact Our Team</h1>
 
 				<div class="div0">
-					<div class="div1">
-						<div class="div2">
+					<div class="div1Tal">
+						<div class="div2Tal">
 							<img id="tal" src="../images/tal.jpeg" alt="tal" >
 							<div class="div3">
 								<center>
@@ -29,6 +44,9 @@
 							<p class="id">315450569</p>
 							<p>Tal 27 years old, a third year student.</p>
 							<p>tal.ben.hamou@post.bgu.ac.il</p>
+							<div style="text-align: right; padding-right: 60px;color:lightseagreen">
+								<label>My First Website</label>
+							</div>
 							<div class="socialLogo">
 								<a href="	https://www.instagram.com/tal.ben.hamo/">
 								<img class="socialMedia" src="../images/instaLogo.png" alt="Instagram">
@@ -36,6 +54,9 @@
 							
 								<a href="https://www.facebook.com/tal.benhamou.71">
 								<img class="socialMedia" src="../images/facebookLogo.png" alt="Facebook">
+								</a>
+								<a href="https://web-development-environments-2022.github.io/315450569/">
+									<img class="linkWeb" src="../images/43-512.webp" >
 								</a>
 						
 							</div>
@@ -53,6 +74,9 @@
 							<p class="id">316586908</p>
 							<p>Ido 26 years old, a third year student.</p>
 							<p>ido3@post.bgu.ac.il</p>
+							<div style="text-align: right; padding-right: 60px;color:lightseagreen">
+								<label>My First Website</label>
+							</div>
 							<div class="socialLogo">
 								<a href="https://www.instagram.com/idocohen32/">
 								<img class="socialMedia" src="../images/instaLogo.png" alt="Instagram">
@@ -61,7 +85,9 @@
 								<a href="https://www.facebook.com/ido.cohen.560">
 								<img class="socialMedia" src="../images/facebookLogo.png" alt="Facebook">
 								</a>
-						
+								<a href="https://web-development-environments-2022.github.io/316586908/">
+									<img class="linkWeb" src="../images/43-512.webp" >
+								</a>
 							</div>
 
 							</center>
@@ -81,7 +107,21 @@ export default {
 </script>
 
 <style>
-
+.pacManIc{
+	width: 100px;
+	height: 100px;
+}
+.pacManIc:hover{
+	zoom: 1.15;
+}
+.socialMedia{
+	width: 100px;
+	height: 100px;
+}
+.linkWeb{
+	width: 100px;
+	height: 100px;
+}
 .div0 {
     background-color: #232323;
     display: flex;
@@ -90,16 +130,32 @@ export default {
 
 .div1 {
     float: left;
+	margin-right: 190px;
     width: 48%;
     margin-bottom: 16px;
     padding: 11px;
 }
 
+.div1Tal {
+    float: left;
+	margin-left: 350px;
+    width: 48%;
+    margin-bottom: 16px;
+    padding: 11px;
+}
+
+
 .div2 {
+	display: inline-block;
     box-shadow: 0px 8px 10px 3px rgba(255, 255, 255, 0.2);
     margin: 4px;
 }
 
+.div2Tal {
+	display: inline-block;
+    box-shadow: 0px 8px 10px 3px rgba(255, 255, 255, 0.2);
+    margin: 4px;
+}
 
 .aboutSec {
     padding: 55px;
@@ -126,8 +182,8 @@ export default {
 
 #ido, #tal {
     float: left;
-    width:  665px;
-    height: 600px;
+    width:  475px;
+    height: 500px;
     max-width: 100%;
     max-height: 100%;
     object-fit: cover;
@@ -155,7 +211,19 @@ export default {
     background-color: rgba(56, 145, 97, 0.4); 
 }  */
 
+#about_page{
+
+    width: 100%; 
+    height: 100%; 
+    max-width: 100%;
+    max-height: 100%;
+    overflow: auto; 
+    /* background-color: rgb(243, 243, 243);  */
+    background-color: rgba(1, 22, 11, 0.4); 
+	}
+
 .aboutClass-inner {
+
     position: relative;
     margin: auto;
     border: 0px solid #888;
