9163e1299d5a7dafc4e214101100157522012f25
Tal <benhatal@post.bgu.ac.il>
2022-08-07 19:16:11 +0300

    push_7/8-bonus

--- src/components/CreateRecipeModal.vue
+++ src/components/CreateRecipeModal.vue
@@ -118,21 +118,67 @@
             required
           ></b-form-input>
         </b-form-group>
+        </form>
         <b-form-group
           label="Instructions"
           label-for="instructions"
           invalid-feedback="instructions is required"
-          :state="nameState"
         >
-          <textarea
-            id="instructions"
-            v-model="instructions"
-            :state="nameState"
+        <h5>Step {{this.steps.length + 1}}</h5>
+          <b-form-group
+          label="Equipments"
+          label-for="Equipments"
+          invalid-feedback="Equipments is required"
+        >
+          <b-form-input
+            id="equipment_name"
+            v-model="equipment_name"
+            placeholder="name of equipment"
+            style="width: 170px; display:inline;"
+          ></b-form-input>
+          <b-form-input
+            id="equipment_tmp_num"
+            v-model="equipment_tmp_num"
+            placeholder="tmp"
+            style="width: 100px;display:inline;margin-left:10px "
+          ></b-form-input>
+          <b-form-select v-model="equipment_tmp_unit" :options="options_tmp_units" style="width:150px; margin-left:10px; height:40px; margin-bottom:5px"></b-form-select>
+          <b-button @click="add_equipments" style="width: 70px;display:inline;margin-left:0px ">Add</b-button>
+        </b-form-group>
+        <b-form-group>
+        <b-form-input
+            id="step_ing_name"
+            v-model="step_ing_name"
+            placeholder="name of ingredient"
+            style="width: 200px; display:inline;"
+          ></b-form-input>
+        <b-button @click="add_ingredients" style="width: 70px;display:inline;margin-left:0px ">Add</b-button>
+        </b-form-group>
+        <br>
+        <b-form-group>
+        <textarea
+            id="step_description"
+            placeholder="Description Of The Step"
+            v-model="step_description"
             required
             style="width:400px;height:100px"
           ></textarea>
+          </b-form-group>
+          <b-form-group
+          label="Preparation time of the step (Optional)"
+          label-for="Length"
+          >
+          <b-form-input
+            id="length_num"
+            v-model="length_num"
+            placeholder="minutes"
+            style="width: 150px; display:inline;"
+          ></b-form-input>
+          </b-form-group>
+
+         <b-button @click="add_step" style="width: 100px;display:inline;margin-left:0px ">Add Step</b-button>
         </b-form-group>
-      </form>
+      
     </b-modal>
   </div>
 </template>
@@ -173,8 +219,21 @@ export default {
           { value: 'ml', text: 'milliliters' },
           { value: 'oz', text: 'ounces' }
         ],
+        options_tmp_units: [
+          { value: 'celsius', text: 'celsius' },
+          { value: 'fahrenheit', text: 'fahrenheit' }
+        ],
         nameState: null,
-        submittedNames: []
+        submittedNames: [],
+        equipment_name: '',
+        equipment_tmp_num: '',
+        equipment_tmp_unit: '',
+        equipments: [],
+        step_ingredients: [],
+        step_ing_name: '',
+        step_description: '',
+        length_num: '',
+        steps: []
       }
     },
     methods: {
@@ -242,12 +301,13 @@ export default {
                   vegan: this.vegan,
                   vegetarian: this.vegetarian,
                   glutenFree: this.gluten,
-                  instructions: this.instructions,
+                  instructions: this.steps,
                   ingredients: this.ingredients,
                   servings: this.servings
                 }
               }
           );
+          this.steps = [];
           // console.log(this.response.data, "HERLLO");
           // console.log("response.status", this.response.status, this.response.length);
           if (this.response.status !== 200) this.$router.replace("/NotFound");
@@ -271,6 +331,40 @@ export default {
         this.ing_name = '';
         this.ing_amount = '';
         this.ing_unit = '';
+      },
+      add_equipments(){
+        this.equipments.push({
+          name: this.equipment_name,
+          tmp: {
+            number: this.equipment_tmp_num,
+            unit: this.equipment_tmp_unit
+          }
+        });
+        this.equipment_name = '';
+        this.equipment_tmp_num = '';
+        this.equipment_tmp_unit = '';
+      },
+      add_ingredients(){
+        this.step_ingredients.push({
+          name: this.step_ing_name
+        });
+        this.step_ing_name = '';
+      },
+      add_step(){
+        this.steps.push({
+          equipments: this.equipments,
+          ingredients: this.step_ingredients,
+          number: this.steps.length + 1,
+          step: this.step_description,
+          length: {
+            number: this.length_num,
+            unit: 'minutes'
+          }
+        });
+        this.equipments = [];
+        this.step_ingredients = [];
+        this.step_description = '';
+        this.length_num = '';
       }
     }
   }
